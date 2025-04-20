a8cf4f7927cdb1084e2a61533879afff0947be1f
Mary Guillemard <mary@mary.zone>
2025-04-20 19:24:40 +0200

    handle MME 120 macro properly
    Signed-off-by: Mary Guillemard <mary@mary.zone>

--- qmd_parser.py
+++ qmd_parser.py
@@ -4,18 +4,23 @@ import ctypes
 
 from ada.hw.compute_a_qmd import *
 
-
+# For raw QMD load
+# regex = r"\.V\ \=\ \((?P<raw_data>.*)\)\n"
 regex = r"\.VALUE\ \=\ (?P<raw_data>.*)\n"
 
 matches = re.finditer(regex, open(sys.argv[1], "r").read(), re.MULTILINE)
 
 raw_bin = bytearray()
 
-for matchNum, match in enumerate(matches, start=1):
+for matchNum, match in enumerate(matches):\
+    # Skip the first 3 dwords (args of the MME macro)
+    if matchNum < 3:
+        continue
     raw_val = match.group('raw_data')
     val = int(raw_val, 16)
-
     raw_bin += val.to_bytes(4, byteorder='little')
+
+assert len(raw_bin) == 256
 
 with open(sys.argv[2], "wb") as f:
     f.write(raw_bin)
